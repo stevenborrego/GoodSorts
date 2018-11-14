7de279d731212b8e426535738eef4f209afbd61a
Steven Borrego <stevenb130@gmail.com>
2018-11-13 18:43:15 -0800

    ass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,45 +4,48 @@ import java.util.Random;
 
 public class Main {
 
-    public static void SelectionSort() {
+    public static void SelectionSort(int [] array) {
 
     }
 
-    public static void MergeSort() {
+    public static void MergeSort(int [] array) {
 
     }
 
-    public static void QuickSort() {
+    public static void QuickSort(int [] array) {
 
     }
 
-    public static void Initialize() {
+    public static void Initialize(int [] smallArray, int [] mediumArray, int [] largeArray) {
 
         Random rand = new Random();
-
-        int [] smallArray = new int [100];
-        int [] mediumArray = new int [1000];
-        int [] largeArray = new int [10000];
 
         // initializes arrays with random int 0-99
         for(int i = 0; i < 100; ++i){
            smallArray[i] = rand.nextInt(100);
         }
+        System.out.print("The Small array has been initialized with 100 integers.\n");
 
         for(int i = 0; i < 1000; ++i){
             mediumArray[i] = rand.nextInt(100);
         }
+        System.out.print("The Medium array has been initialized with 1,000 integers.\n");
 
         for(int i = 0; i < 10000; ++i){
             largeArray[i] = rand.nextInt(100);
         }
+        System.out.print("The Large array has been initialized with 10,000 integers.\n");
     }
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.print("Welcome to ASS");
+        System.out.print("Welcome to ASS\n");
 
-        Initialize();
+        int [] smallArray = new int [100];
+        int [] mediumArray = new int [1000];
+        int [] largeArray = new int [10000];
+
+        Initialize(smallArray, mediumArray, largeArray);
     }
 }
